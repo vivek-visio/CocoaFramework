2872f8d628e2b0ac7ee4378410d576efcb64aacd
Vivek <vivek@visio-apps.com>
2024-09-09 12:46:48 +0530

    Initial Commit

---
+++ CocoaFramework/CocoaFramework.h
@@ -0,0 +1,18 @@
+//
+//  CocoaFramework.h
+//  CocoaFramework
+//
+//  Created by Visioapps on 09/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaFramework.
+FOUNDATION_EXPORT double CocoaFrameworkVersionNumber;
+
+//! Project version string for CocoaFramework.
+FOUNDATION_EXPORT const unsigned char CocoaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaFramework/PublicHeader.h>
+
+
